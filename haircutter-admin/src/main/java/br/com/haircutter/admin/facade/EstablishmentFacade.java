8c1a32f0a7ba4110fa3ef35d8386ea53fd650616
henrique <henriquesdfaria@gmail.com>
2016-04-07 20:13:09 -0300

    WIP establishment edit profile

--- haircutter-admin/src/main/java/br/com/haircutter/admin/facade/EstablishmentFacade.java
+++ haircutter-admin/src/main/java/br/com/haircutter/admin/facade/EstablishmentFacade.java
@@ -1,6 +1,12 @@
 package br.com.haircutter.admin.facade;
 
+import br.com.haircutter.admin.endpoint.EstablishmentEndpoint;
+import br.com.haircutter.admin.facade.json.EstablishmentJson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,4 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api")
 public class EstablishmentFacade {
+
+    @Autowired
+    private EstablishmentEndpoint endpoint;
+
+    @RequestMapping(value = {"/establishment-admin/establishment/edit"}, method = RequestMethod.POST)
+    public ResponseEntity<?> createNewRequest(@RequestBody EstablishmentJson establishmentJson) {
+
+        return ResponseEntity.ok(endpoint.edit(establishmentJson));
+    }
 }
