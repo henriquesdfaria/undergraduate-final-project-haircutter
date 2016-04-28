cdc0c62574120d2a01b10b6f573246135b0372da
Henrique Faria <henriquesdfaria@gmail.com>
2016-04-28 19:02:45 -0300

    Establishment Services Usecase

--- haircutter-admin/src/main/resources/templates/app/manager/js/app.js
+++ haircutter-admin/src/main/resources/templates/app/manager/js/app.js
@@ -3,32 +3,33 @@
 /* App Module */
 
 var managerApp = angular.module('managerApp', [
-        'ngRoute',
-        'managerControllers'
-    ]
+    'ngRoute',
+    'managerControllers',
+    'ui.utils.masks'
+  ]
 );
 
 managerApp.config(['$routeProvider',
-        function ($routeProvider) {
-            $routeProvider
-                .when('/establishment/services', {
-                        templateUrl: 'manager/partials/establishment-services-content.html',
-                        controller: 'EstablishmentServicesController'
-                    }
-                )
-                .when('/establishment/services/service/:establishmentServiceId', {
-                        templateUrl: 'manager/partials/establishment-service-content.html',
-                        controller: 'EstablishmentServiceController'
-                    }
-                )
-                .when('/establishment/services/create', {
-                        templateUrl: 'manager/partials/create-establishment-service-content.html',
-                        controller: 'CreateEstablishmentServiceController'
-                    }
-                )
-                .otherwise(
-                    {redirectTo: '/establishment/services'}
-                );
-        }
-    ]
+    function ($routeProvider) {
+      $routeProvider
+        .when('/establishment/services', {
+            templateUrl: 'manager/partials/establishment-services-content.html',
+            controller: 'EstablishmentServicesController'
+          }
+        )
+        .when('/establishment/services/service/:establishmentServiceId', {
+            templateUrl: 'manager/partials/establishment-service-content.html',
+            controller: 'EstablishmentServiceController'
+          }
+        )
+        .when('/establishment/services/create', {
+            templateUrl: 'manager/partials/create-establishment-service-content.html',
+            controller: 'CreateEstablishmentServiceController'
+          }
+        )
+        .otherwise(
+          {redirectTo: '/establishment/services'}
+        );
+    }
+  ]
 );
